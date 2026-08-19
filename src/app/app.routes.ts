092498d9b4e71368c6b3773378df3e64b6e7cdbc
robin <ruzza.robin@gmail.com>
2026-08-19 18:53:27 +0200

    Initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Songs } from './components/songs/songs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'songs', component: Songs },
+];
